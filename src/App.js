c43365373dd92ab4d94ddda3028b0ee62631ef32
korkt-kim <gnscott6@naver.com>
2021-11-02 14:49:30 +0900

    make useConfirm customhook
    - confirm 창 생성 확인/취소 시 상황에 맞는 callback 함수 실행

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import {useState,useEffect, useRef} from 'react';
+import React,{useState,useEffect, useRef} from 'react';
+import ReactDOM from 'react-dom'
 
-const useClick = (onClick) =>{
-  const element = useRef();
-  useEffect(()=>{
-    if(element.current){
-      element.current.addEventListener('click',onClick)
+
+const useConfirm = (message="", onConfirm, onReject) =>{
+  if(typeof onConfirm !=='function'){
+    return;
+  }
+  const confirmAction = () =>{
+    // eslint-disable-next-line no-restricted-globals
+    if(confirm(message)){
+      onConfirm();
+    }else{
+      onReject();
     }
-  })
-  return element;
-  
+  }
+  return confirmAction;
 }
 
 function App() {
-  const sayHello = () => console.log('say Hello')
-  const title = useClick(sayHello)
-
+  const deleteWorld = () => console.log('deleting the world')
+  
+  const abort = () => console.log('Aborted');
+  const confirmDelete = useConfirm("Are you sure?",deleteWorld,abort);
   return (
     <div >
-      <h1 ref={title}>Hi</h1>
+      <button onClick={confirmDelete}>Delete the world</button>
     </div>
   )
 }
