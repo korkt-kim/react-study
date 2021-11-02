d69526d87bec49581efaa19b7b2e5f77dcb7b17a
korkt-kim <gnscott6@naver.com>
2021-11-02 09:48:17 +0900

    make useInput custom hook
    - 숫자만 입력가능한 input type text custom hook 생성

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+
+const useInput = (initialValue,validator)=>{
+  const [value,setValue] = useState(initialValue);
+  const onChange = ({target:{value}}) =>{
+    if(typeof validator ==='function' && validator(value)){
+      setValue(value)
+    }else{
+      alert("must be numeric")
+    }
+  }
+  return {value,onChange}
+}
 
 function App() {
+  const myValidator = (str) => !isNaN(str)
+  const name = useInput(1,myValidator)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <input type ="text" {...name}/>
   );
 }
 
