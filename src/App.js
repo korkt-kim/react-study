064df7617dfa5ea9b62d73e0cb11279159c7f40a
korkt-kim <gnscott6@naver.com>
2021-11-03 22:03:07 +0900

    React.memo and useMemo and useCallback
    - React.memo : 상위 컴포넌트가 갱신되어도 하위 컴포넌트의 props나 state에 변화가 없어 rerender가 필요없을때 rerender 안함
    - useMemo : 2번째 parameter로 넘긴 변수가 변하지 않는 이상 rerender되어도 computed 변수를 다시 계산하지 않는다
    - useCallback : 2번째 paramter로 내부에서 사용하는 state, props를 넣어준다.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,60 @@
 import React,{useState,useEffect, useRef} from 'react';
 import ReactDOM from 'react-dom'
-import Modal from './Modal.js'
-import useModal from './useModal.js'
 import './App.css';
 
 
-function App() {
-  const {isShowing, toggle} = useModal();
+import Info from "./Info";
+
+const App = () => {
+  const [color, setColor] = useState("");
+  const [movie, setMovie] = useState("");
+
+  const onChangeHandler = e => {
+    if (e.target.id === "color") setColor(e.target.value);
+    else setMovie(e.target.value);
+  };
+
   return (
-    <div>
-      <button className="button-default" onClick={toggle}>Show Modal</button>
-      <Modal
-        isShowing={isShowing}
-        hide={toggle}
-      />
+    <div className="App">
+      <div>
+        <label>
+          What is your favorite color of rainbow ?
+          <input id="color" value={color} onChange={onChangeHandler} />
+        </label>
+      </div>
+      <div>
+        What is your favorite movie among these ?
+        <label>
+          <input
+            type="radio"
+            name="movie"
+            value="Marriage Story"
+            onChange={onChangeHandler}
+          />
+          Marriage Story
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="movie"
+            value="The Fast And The Furious"
+            onChange={onChangeHandler}
+          />
+          The Fast And The Furious
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="movie"
+            value="Avengers"
+            onChange={onChangeHandler}
+          />
+          Avengers
+        </label>
+      </div>
+      <Info color={color} movie={movie} />
     </div>
-  )
-}
+  );
+};
+
 export default App;
